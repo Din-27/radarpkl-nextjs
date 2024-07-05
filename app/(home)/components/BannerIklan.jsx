8480f790fda45e4d13bded0ec95323bcda716d48
herdin27 <it06.knitto@gmail.com>
2024-07-05 15:42:22 +0700

    fix LCP

--- app/(home)/components/BannerIklan.jsx
+++ app/(home)/components/BannerIklan.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function BannerIklan({ src }) {
   return (
     <div className="flex justify-center w-full md:p-4">
-      <Image
+      {src !== 'iklan_banner_dua' ? < Image
         src={src}
         width={1200}
         height={800}
@@ -15,6 +15,8 @@ export default function BannerIklan({ src }) {
         sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
         className="w-screen"
       />
+        :
+        <div className="w-full h-32 bg-white" />}
     </div>
   );
 }
